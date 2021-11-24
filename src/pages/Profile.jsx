2a550b2b90cbd4b1657f1933d290709c1d8ba8ef
dwdjjj <dwdjjj@naver.com>
2021-11-24 23:46:01 +0900

    React-JSX 링크이동(1)까지

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+export default function Profile(props) {
+  const id = props.match.params.id;
+  console.log(id, typeof id);
+  // console.log(props);
+  return (
+    <div>
+      <h2>Profile 페이지입니다.</h2>
+      {id && <p>id는 {id}입니다.</p>}
+    </div>
+  );
+}
